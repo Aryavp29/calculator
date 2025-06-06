31c6039ace07cfe5764b4b76249034178ec1e096
Arya V P <29aryaprathab@gmail.com>
2025-06-06 11:03:11 +0530

    functions added

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function buttonClick(number) {
-console.log(number);
+document.getElementById('screenid').value+=number;
+}
+
+function calculator(){
+    var value=document.getElementById('screenid').value;
+    try{
+    document.getElementById('screenid').value=eval(value);
+    }
+    catch{
+        document.getElementById('screenid').value="Error";
+    }
+}
+function clearScreen(){
+    document.getElementById('screenid').value = "";
 }
